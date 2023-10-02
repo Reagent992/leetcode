9de415d8445ce73fe0bf190f9a8d18a78e851ec6
Miron Sadykov <reagent992@gmail.com>
2023-10-02 22:41:23 +0300

    solved the problem

--- problems/21.py
+++ problems/21.py
@@ -1,36 +1,47 @@
 """
 21. Merge Two Sorted Lists
 https://leetcode.com/problems/merge-two-sorted-lists/
-Status: # TODO: Not Solved.
+Status: Solved.
 """
 
 
-# Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-class Solution(object):
-    def mergeTwoLists(self, list1, list2=None):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        while list1:
-            print(list1.val, end=" -> ")
-            list1 = list1.next
-        print("None")
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        tail = head = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                tail = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                tail = list2
+                list2 = list2.next
+        if list1 or list2:
+            tail.next = list1 if list1 else list2
+        return head.next
 
 
 if __name__ == '__main__':
-    list1 = [1, 2, 4]
-    first_node = ListNode(list1[-1])
-    last_node = first_node
-    for i in list1:
-        t = ListNode(i, last_node)
-        last_node = t
+    n3 = ListNode(val=4)
+    n2 = ListNode(val=2, next=n3)
+    n1 = ListNode(val=1, next=n2)
+    l3 = ListNode(val=4)
+    l2 = ListNode(val=3, next=l3)
+    l1 = ListNode(val=1, next=l2)
 
-    print(Solution().mergeTwoLists(list1=last_node))
+
+    def print_linked_list(vertex):
+        while vertex:
+            print(vertex.val, end=" -> ")
+            vertex = vertex.next
+        print("The End")
+
+
+    print_linked_list(Solution().mergeTwoLists(list1=n1, list2=l1))
